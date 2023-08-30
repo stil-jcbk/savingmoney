f497243b4793ccbfca9c9c836929efdad4eb86ba
Jakub Konieczny <53277197+stil-jcbk@users.noreply.github.com>
2023-08-31 00:01:58 +0200

    added footer

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../../components/navbar/navbar";
+import Footer from "../../components/footer/footer";
 
 export default function Layout() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
