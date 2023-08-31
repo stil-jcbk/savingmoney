6d57b0c8a2eb5d92bb70ed65b33df02d0161b62e
Jakub Konieczny <53277197+stil-jcbk@users.noreply.github.com>
2023-08-31 22:28:45 +0200

    layout fix

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,10 +1,12 @@
 import Navbar from "../../components/navbar/navbar";
 import Footer from "../../components/footer/footer";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <div>
       <Navbar />
+      <Outlet />
       <Footer />
     </div>
   );
