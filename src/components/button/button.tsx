04fc7732a7b122c35b1a6caa7832a2a676606a05
stil-jcbk <kubkon134@gmail.com>
2023-09-11 16:49:07 +0200

    added className property to Button component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,15 +3,24 @@ import "./style.css";
 type ButtonProps = {
   children: string;
   click: Function;
+  className?: string;
 };
 
 export default function Button(props: ButtonProps) {
+  props.className = "";
   return (
     <button
       onClick={() => {
         props.click();
       }}
-      className="btn"
+      className={
+        "btn" +
+        (props.className.length > 0
+          ? () => {
+              return " " + props.className;
+            }
+          : "")
+      }
     >
       {props.children}
     </button>
